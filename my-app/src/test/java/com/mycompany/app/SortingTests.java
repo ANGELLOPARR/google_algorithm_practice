df73d2a066cac97bf1ecc3f9de54f25916e2391d
ANGELLOPARR <angellop123@gmail.com>
2020-01-07 02:45:26 -0800

    add more mergesort tests

--- my-app/src/test/java/com/mycompany/app/SortingTests.java
+++ my-app/src/test/java/com/mycompany/app/SortingTests.java
@@ -8,9 +8,36 @@ import java.util.*;
 public class SortingTests 
 {
     @Test
-    public void shouldAnswerWithTrue()
+    public void MergesortTest01()
     {
         int[] inputList = { 1, 7, 5, 8, 4 };
+        int[] mySortedList = Mergesort.sort(inputList);
+        Arrays.sort(inputList);
+        assertArrayEquals(inputList, mySortedList);
+    }
+
+    @Test
+    public void MergesortTest02()
+    {
+        int[] inputList = {};
+        int[] mySortedList = Mergesort.sort(inputList);
+        Arrays.sort(inputList);
+        assertArrayEquals(inputList, mySortedList);
+    }
+
+    @Test
+    public void MergesortTest03()
+    {
+        int[] inputList = { 9, 9, 9, 9, 9 };
+        int[] mySortedList = Mergesort.sort(inputList);
+        Arrays.sort(inputList);
+        assertArrayEquals(inputList, mySortedList);
+    }
+
+    @Test
+    public void MergesortTest04()
+    {
+        int[] inputList = { -100, -50, 50, 13, -14 };
         int[] mySortedList = Mergesort.sort(inputList);
         Arrays.sort(inputList);
         assertArrayEquals(inputList, mySortedList);
